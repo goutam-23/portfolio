e09d54dcc71936c9e291f4a8671a238625b70d11
goutam-23 <69918713+goutam-23@users.noreply.github.com>
2023-05-10 21:33:26 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
+import Header from './components/header/header'
+import Nav from'./components/nav/nav'
+import About from './components/about/about'
+import Contact from './components/contact/contact'
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import Services from './components/services/services'
+import Testimonials from './components/testimonials/testimonials'
+import Footer from './components/footer/footer'
 
-function App() {
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+     <Header/>
+     <Nav/>
+     <About/>
+     <Experience/>
+     <Services/>
+     <Portfolio/>
+     <Testimonials/>
+     <Contact/>
+     <Footer/>
+
+     </>
   );
 }
 
